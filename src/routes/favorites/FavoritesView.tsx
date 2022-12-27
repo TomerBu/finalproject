7c7c19512c09682aa0f43487ff9937ddd9cac9c5
BuTomer <bu.tomer@gmail.com>
2022-12-27 15:53:51 +0200

    favorites view

--- src/routes/favorites/FavoritesView.tsx
+++ src/routes/favorites/FavoritesView.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useAppSelector } from "../../app/hooks";
+import NewsItem from "../news/NewsItem";
 
 const FavoritesView = () => {
+  const { articles } = useAppSelector((state) => state.news);
+  const favoriteArticles = articles.filter((a) => a.isFavorite);
   return (
-    <div>FavoritesView</div>
-  )
-}
+    <div>
+      <h2 className="text-center">Favorites: </h2>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        {favoriteArticles.map((a) => (
+          <NewsItem key={a.url} {...a} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default FavoritesView
+export default FavoritesView;
